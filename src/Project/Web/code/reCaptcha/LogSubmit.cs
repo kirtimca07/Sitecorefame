6714a7545805efe4badce8d0e382626fb64b2321
Kirti Kumar <kirti.kumar@altudo.co>
2022-04-30 23:25:07 +0530

    Recaptcha

---
+++ src/Project/Web/code/reCaptcha/LogSubmit.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Processing;
+using Sitecore.ExperienceForms.Processing.Actions;
+using static System.FormattableString;
+
+namespace AdvanceSitecore.Project.Web.reCaptcha
+{
+    public class LogSubmit : SubmitActionBase<string>
+    {
+        public LogSubmit(ISubmitActionData submitActionData) : base(submitActionData) { }
+        protected override bool TryParse(string value, out string target) { target = string.Empty; return true; }
+        protected override bool Execute(string data, FormSubmitContext formSubmitContext)
+        {
+            Assert.ArgumentNotNull(formSubmitContext, nameof(formSubmitContext));
+
+            if (!formSubmitContext.HasErrors)
+            {
+                Logger.Info(Invariant($"Form {formSubmitContext.FormId} submitted successfully."), this);
+            }
+            else
+            {
+                Logger.Warn(Invariant($"Form {formSubmitContext.FormId} submitted with errors: {string.Join(", ", formSubmitContext.Errors.Select(t => t.ErrorMessage))}."), this);
+            }
+
+            return true;
+        }
+    }
+}
